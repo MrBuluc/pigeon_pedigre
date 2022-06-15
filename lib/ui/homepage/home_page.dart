b40e1dda1c1d63f092d96a6a85021eca9f7f4c15
U180709061 <hakkicanbuluc@gmail.com>
2022-06-15 16:34:09 +0300

    project completed

--- lib/ui/homepage/home_page.dart
+++ lib/ui/homepage/home_page.dart
@@ -14,12 +14,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final Stream<QuerySnapshot> pigeonsStream =
+  Stream<QuerySnapshot> pigeonsStream =
       FirebaseFirestore.instance.collection("Pigeons").snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text("Pigeons"),
         centerTitle: true,
@@ -35,36 +36,70 @@ class _HomePageState extends State<HomePage> {
               MaterialPageRoute(builder: (context) => const AddPigeonPage()));
         },
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: pigeonsStream,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return const Text("Something went wong");
-          }
+      body: Column(
+        children: [
+          Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 15, top: 10),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.search),
+                    hintText: "Search...",
+                  ),
+                  onChanged: (String searchId) => search(searchId),
+                ),
+              )),
+          Expanded(
+            flex: 10,
+            child: StreamBuilder<QuerySnapshot>(
+              stream: pigeonsStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return const Text("Something went wong");
+                }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Text("Loaing");
-          }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Text("Loaing");
+                }
 
-          return ListView(
-            children: snapshot.data!.docs.map((DocumentSnapshot document) {
-              Map<String, dynamic> data =
-                  document.data()! as Map<String, dynamic>;
-              return ListTile(
-                title: Text(data["id"]),
-                leading: const Icon(Icons.flutter_dash),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              DetailPage(pigeon: Pigeon.fromJson(data))));
-                },
-              );
-            }).toList(),
-          );
-        },
+                return ListView(
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return ListTile(
+                      title: Text(data["id"]),
+                      leading: const Icon(Icons.flutter_dash),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    DetailPage(pigeon: Pigeon.fromJson(data))));
+                      },
+                    );
+                  }).toList(),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  search(String searchId) {
+    if (searchId.isNotEmpty) {
+      pigeonsStream = FirebaseFirestore.instance
+          .collection("Pigeons")
+          .where("id", isEqualTo: searchId)
+          .snapshots();
+    } else {
+      pigeonsStream =
+          FirebaseFirestore.instance.collection("Pigeons").snapshots();
+    }
+    setState(() {});
   }
 }
